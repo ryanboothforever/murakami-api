299649c369f2d73e0ad70417343cdf52b641bd82
Ryan Booth <ryanboothforever@gmail.com>
2021-03-25 08:25:39 -0500

    added port for heroku

--- index.js
+++ index.js
@@ -46,6 +46,6 @@ app.get('/api/books/:bookTitle', (request, response) =>{
     }
 })
 
-app.listen( PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server is running on port: ${PORT}`);
 })
